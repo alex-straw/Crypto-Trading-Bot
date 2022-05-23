1156b88df9090b0b5acad8a8422320de76b0ed10
alex-straw <as17163@bristol.ac.uk>
2022-05-23 18:53:33 +0000

    began trying to access curve features but having some issue with 'next' - perhaps find an alternative as it doesn't seem particularly optimised in this way

--- algo/main.py
+++ algo/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import cbpro_api
 import plot_lob
-import bisect
 import numpy as np
 
 
@@ -57,7 +56,28 @@ def clean_lob(_lob, middle_pct):
     cleaned_lob = normalise_lob_qtys(cleaned_lob)
     cleaned_lob = normalise_prices(cleaned_lob, market_price)
 
-    return cleaned_lob
+    return cleaned_lob, market_price
+
+
+def get_curve_features(cleaned_lob, n_points_per_curve, middle_pct):
+
+    bids_bins = np.linspace(1, 1-(middle_pct/2)+0.01, n_points_per_curve)
+    asks_bins = np.linspace(1, 1+(middle_pct/2)-0.01, n_points_per_curve)
+
+    print(bids_bins)
+
+    feature_dict = {}
+
+    for bid_pct in bids_bins:
+        bid_bin_idx = next(x for x, val in enumerate(cleaned_lob['bids_prices']) if val < bid_pct)
+        print(bid_bin_idx)
+        feature_dict[bid_pct] = cleaned_lob['bids_cumsum_qtys'][bid_bin_idx]
+
+    for ask_pct in asks_bins:
+        ask_pct_idx = next(x for x, val in enumerate(cleaned_lob['bids_prices']) if val > ask_pct)
+        feature_dict[ask_pct] = cleaned_lob['asks_cumsum_qtys'][ask_pct_idx]
+
+    return feature_dict
 
 
 def main():
@@ -67,10 +87,19 @@ def main():
         'level': 2
     }
 
+    middle_pct = 0.1
+    n_points_per_curve = 9
+
     lob = cbpro_api.get_lob(api_request['product'], api_request['level'])
-    cleaned_lob = clean_lob(lob, middle_pct=0.05)
+    cleaned_lob, market_price = clean_lob(lob, middle_pct)
 
     plot_lob.plot_limit_order_book(cleaned_lob)
+
+    # feature_dict = get_curve_features(cleaned_lob, n_points_per_curve, middle_pct)
+
+    # feature_dict[market_price] = market_price
+
+    # print(feature_dict)
 
 
 if __name__ == "__main__":
