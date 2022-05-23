ba4488fab6d184341d3dc19c5590656df04f426c
alex-straw <as17163@bristol.ac.uk>
2022-05-23 17:40:35 +0000

    added some labels and a title to the LOB plot

--- algo/plot_lob.py
+++ algo/plot_lob.py
@@ -3,11 +3,13 @@ import numpy as np
 
 
 def plot_limit_order_book(lob_dict):
-
+    """ Plots a limit order book """
     fig, ax = plt.subplots()
-
     ax.plot(lob_dict['asks_prices'], lob_dict['asks_cumsum_qtys'], label='asks')
     ax.plot(lob_dict['bids_prices'], lob_dict['bids_cumsum_qtys'], label='bids')
     ax.grid()
-
+    ax.legend()
+    plt.title('Limit Order Book Plot')
+    ax.set_xlabel('Price ($)')
+    ax.set_ylabel('Cumulative Quantity')
     plt.show()
