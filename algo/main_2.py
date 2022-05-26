4ca1011bf6411111b25075286510fdfb1b4a5133
alex-straw <as17163@bristol.ac.uk>
2022-05-26 14:20:29 +0000

    calculating market price estimate

--- algo/main_2.py
+++ algo/main_2.py
@@ -1,15 +1,26 @@
 import cbpro_api
 
 
+def get_market_price(lob):
+    """ Midpoint of bid-ask spread """
+    best_ask = float(lob['asks'][0][0])
+    best_bid = float(lob['bids'][0][0])
+    return (best_bid+best_ask)/2
+
+
 def main():
     api_request = {
         'product': 'BTC-USD',
         'level': 2
     }
 
+    # Want to estimate the b
+
     lob = cbpro_api.get_lob(api_request['product'], api_request['level'])
 
-    print(lob)
+    market_price = get_market_price(lob)
+
+    print(market_price)
 
 
 if __name__ == "__main__":
