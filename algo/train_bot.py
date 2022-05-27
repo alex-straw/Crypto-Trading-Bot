de9b7a9dab6e9dd00e39c19ebbeb9246ebdfaaba
alex-straw <as17163@bristol.ac.uk>
2022-05-27 14:29:56 +0000

    added linear regression model to predict future price movements in the next 30 seconds

--- algo/train_bot.py
+++ algo/train_bot.py
@@ -1,21 +1,24 @@
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
 from numpy import concatenate
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 
 def add_future_price(df, n_steps_ahead):
     """ Get future price change for training labels """
     df['shifted_market_price'] = df['market_price'].shift(periods=n_steps_ahead)
     df = df.iloc[n_steps_ahead:]  # Skip NaNs
-    df['future_price_change_pct'] = (df['shifted_market_price'] - df['market_price']) / df['market_price']*100
+    df['future_price_change_pct'] = (df['shifted_market_price'] - df['market_price']) / df['market_price']
 
     return df
 
 
 def get_x_y(df):
-
     x_cols = [col for col in df if col.startswith('bid') or col.startswith('ask')]
-
     x_data = df[x_cols]
     y_data = df['future_price_change_pct']
 
@@ -33,20 +36,45 @@ def get_train_test_split(train_size, df):
     return train, test
 
 
+def plot_model(y_test_pred, y_test_true, model):
+    fig, ax = plt.subplots()
+
+    time_data = y_test_true.index
+
+    datapoint_index = range(0, len(time_data))
+
+    ax.plot(datapoint_index, y_test_pred, label='predicted change over next 30 seconds')
+    ax.plot(datapoint_index, y_test_true, label='actual change over next 30 seconds')
+    ax.grid()
+    ax.legend()
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Change (%)')
+    plt.title(model)
+    plt.show()
+
+
 def main():
 
     train_size = 0.8
+    n_steps_ahead = 6
 
     feature_df = pd.read_csv('output_data/BTC-USD_data.csv')
     feature_df = feature_df.drop(feature_df.columns[0], axis=1)  # Drop original index column
+    feature_df['time'] = pd.to_datetime(feature_df['time'], unit='s')
     feature_df.set_index('time', inplace=True, drop=True)
 
-    feature_df = add_future_price(feature_df, n_steps_ahead=6)
+    feature_df = add_future_price(feature_df, n_steps_ahead)
 
     test, train = get_train_test_split(train_size, feature_df)
 
     x_data_train, y_data_train = get_x_y(train)
     x_data_test, y_data_test = get_x_y(test)
+
+    linear_reg_model = LinearRegression().fit(x_data_train, y_data_train)
+
+    y_data_pred = linear_reg_model.predict(x_data_test)
+
+    plot_model(y_data_pred, y_data_test, 'Linear Regression')
 
 
 if __name__ == "__main__":
